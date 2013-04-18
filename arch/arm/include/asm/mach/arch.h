fc1201d6a7cbcfa63f6ebf3d34f31234111519ad
Jon Medhurst <tixy@linaro.org>
2013-04-18 13:54:46 +0000

    ARM: Enable selection of SMP operations at boot time
    Add a new 'smp_init' hook to machine_desc so platforms can specify a
    function to be used to setup smp ops instead of having a statically
    defined value.  The hook must return true when smp_ops are initialized.
    If false the static mdesc->smp_ops will be used by default.
    
    Signed-off-by: Jon Medhurst <tixy@linaro.org>
    Signed-off-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Reviewed-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

--- arch/arm/include/asm/mach/arch.h
+++ arch/arm/include/asm/mach/arch.h
@@ -16,8 +16,10 @@ struct pt_regs;
 struct smp_operations;
 #ifdef CONFIG_SMP
 #define smp_ops(ops) (&(ops))
+#define smp_init_ops(ops) (&(ops))
 #else
 #define smp_ops(ops) (struct smp_operations *)NULL
+#define smp_init_ops(ops) (bool (*)(void))NULL
 #endif
 
 struct machine_desc {
@@ -41,6 +43,8 @@ struct machine_desc {
 	unsigned char		reserve_lp2 :1;	/* never has lp2	*/
 	char			restart_mode;	/* default restart mode	*/
 	struct smp_operations	*smp;		/* SMP operations	*/
+	/* Detect smp_ops dynamically, based on DT */
+	bool			(*smp_init)(void);
 	void			(*fixup)(struct tag *, char **,
 					 struct meminfo *);
 	void			(*reserve)(void);/* reserve mem blocks	*/
