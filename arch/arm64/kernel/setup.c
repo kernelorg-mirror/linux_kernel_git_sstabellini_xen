16992eae71e703121d43c4a462f715bb4bdfe96f
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2013-08-29 18:26:19 +0000

    xen/arm,arm64: move Xen initialization earlier
    Move Xen initialization earlier, before any DMA requests can be made.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    
    
    Changes in v3:
    - add missing __init in xen_early_init declaration.

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -53,6 +53,7 @@
 #include <asm/traps.h>
 #include <asm/memblock.h>
 #include <asm/psci.h>
+#include <asm/xen/hypervisor.h>
 
 unsigned int processor_id;
 EXPORT_SYMBOL(processor_id);
@@ -267,6 +268,7 @@ void __init setup_arch(char **cmdline_p)
 	unflatten_device_tree();
 
 	psci_init();
+	xen_early_init();
 
 	cpu_logical_map(0) = read_cpuid_mpidr() & MPIDR_HWID_BITMASK;
 #ifdef CONFIG_SMP
