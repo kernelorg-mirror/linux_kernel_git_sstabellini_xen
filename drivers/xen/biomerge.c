abe0cb4f0241060122dfa98b03290e1cf6338b2d
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2013-09-26 16:26:23 +0000

    arm,arm64: do not always merge biovec if we are running on Xen
    This is similar to what it is done on X86: biovecs are prevented from merging
    otherwise every dma requests would be forced to bounce on the swiotlb buffer.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- drivers/xen/biomerge.c
+++ drivers/xen/biomerge.c
@@ -2,6 +2,7 @@
 #include <linux/io.h>
 #include <linux/export.h>
 #include <xen/page.h>
+#include <xen/features.h>
 
 bool xen_biovec_phys_mergeable(const struct bio_vec *vec1,
 			       const struct bio_vec *vec2)
@@ -9,7 +10,8 @@ bool xen_biovec_phys_mergeable(const struct bio_vec *vec1,
 	unsigned long mfn1 = pfn_to_mfn(page_to_pfn(vec1->bv_page));
 	unsigned long mfn2 = pfn_to_mfn(page_to_pfn(vec2->bv_page));
 
-	return __BIOVEC_PHYS_MERGEABLE(vec1, vec2) &&
+	return !xen_feature(XENFEAT_auto_translated_physmap) &&
+		__BIOVEC_PHYS_MERGEABLE(vec1, vec2) &&
 		((mfn1 == mfn2) || ((mfn1+1) == mfn2));
 }
 EXPORT_SYMBOL(xen_biovec_phys_mergeable);
