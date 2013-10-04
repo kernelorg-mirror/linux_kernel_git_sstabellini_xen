9cf77a565a8732685bf2c7190cb2e9230009648a
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2013-10-04 17:00:39 +0000

    patch dma_mask_workaorund

--- arch/arm/include/asm/dma-mapping.h
+++ arch/arm/include/asm/dma-mapping.h
@@ -113,9 +113,9 @@ static inline phys_addr_t dma_to_phys(struct device *dev, dma_addr_t dev_addr)
 static inline bool dma_capable(struct device *dev, dma_addr_t addr, size_t size)
 {
 	u64 limit, mask;
-	
-	if (!dev->dma_mask)
-		return 0;
+
+    if (!dev || !dev->dma_mask)
+        return 1;
 
 	mask = *dev->dma_mask;
 
