40c9372c51f8f3e15599adf901df560588740804
Stefano Stabellini <sstabellini@kernel.org>
2017-01-13 14:54:56 -0800

    9pfs Xen transport

---
+++ include/xen/interface/io/9pfs.h
@@ -0,0 +1,111 @@
+/*
+ * 9pfs.h -- Xen 9PFS transport
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Copyright (C) 2016 Stefano Stabellini <stefano@aporeto.com>
+ */
+
+#ifndef __XEN_PUBLIC_IO_9PFS_H__
+#define __XEN_PUBLIC_IO_9PFS_H__
+
+#include "xen/interface/io/ring.h"
+
+#define XEN_9PFS_RING_ORDER (6)
+#define XEN_9PFS_RING_SIZE ((1 << ((XEN_9PFS_RING_ORDER) + XEN_PAGE_SHIFT)) / 2)
+#define MASK_XEN_9PFS_IDX(idx) ((idx) & (XEN_9PFS_RING_SIZE - 1))
+#define _MASK_XEN_9PFS_IDX(idx) ((idx) & (XEN_9PFS_RING_SIZE - 1))
+
+typedef uint32_t XEN_9PFS_RING_IDX;
+
+struct xen_9pfs_header {
+	uint32_t size;
+	uint8_t id;
+	uint16_t tag;
+} __attribute__((packed));
+
+static inline void xen_9pfs_read_header(char *buf,
+		XEN_9PFS_RING_IDX *masked_prod, XEN_9PFS_RING_IDX *masked_cons,
+		struct xen_9pfs_header *h) {
+	if (*masked_cons < *masked_prod) {
+		memcpy(h, buf + *masked_cons, sizeof(*h));
+	} else {
+		if (sizeof(*h) > XEN_9PFS_RING_SIZE - *masked_cons) {
+			memcpy(h, buf + *masked_cons, XEN_9PFS_RING_SIZE - *masked_cons);
+			memcpy((char *)h + XEN_9PFS_RING_SIZE - *masked_cons, buf, sizeof(*h) - (XEN_9PFS_RING_SIZE - *masked_cons));
+		} else {
+			memcpy(h, buf + *masked_cons, sizeof(*h));
+		}
+	}
+	*masked_cons = _MASK_XEN_9PFS_IDX(*masked_cons + sizeof(*h));
+}
+
+static inline void xen_9pfs_write_header(char *buf,
+		XEN_9PFS_RING_IDX *masked_prod, XEN_9PFS_RING_IDX *masked_cons,
+		struct xen_9pfs_header h) {
+	if (*masked_prod < *masked_cons) {
+		memcpy(buf + *masked_prod, &h, sizeof(h));
+	} else {
+		if (sizeof(h) > XEN_9PFS_RING_SIZE - *masked_prod) {
+			memcpy(buf + *masked_prod, &h, XEN_9PFS_RING_SIZE - *masked_prod);
+			memcpy(buf, (char *)(&h) + (XEN_9PFS_RING_SIZE - *masked_prod), sizeof(h) - (XEN_9PFS_RING_SIZE - *masked_prod)); 
+		} else {
+			memcpy(buf + *masked_prod, &h, sizeof(h)); 
+		}
+	}
+	*masked_prod = _MASK_XEN_9PFS_IDX(*masked_prod + sizeof(h));
+}
+
+struct xen_9pfs_data {
+	char *in; /* half of the allocation */
+	char *out; /* half of the allocation */
+};
+
+struct xen_9pfs_data_intf {
+	XEN_9PFS_RING_IDX in_cons, in_prod;
+	XEN_9PFS_RING_IDX out_cons, out_prod;
+
+	uint32_t ring_order;
+	grant_ref_t ref[];
+};
+
+static inline XEN_9PFS_RING_IDX xen_9pfs_ring_queued(XEN_9PFS_RING_IDX prod,
+		XEN_9PFS_RING_IDX cons)
+{
+	XEN_9PFS_RING_IDX size;
+
+	if (prod == cons)
+		return 0;
+
+	prod = _MASK_XEN_9PFS_IDX(prod);
+	cons = _MASK_XEN_9PFS_IDX(cons);
+
+	if (prod == cons)
+		return XEN_9PFS_RING_SIZE;
+
+	if (prod > cons)
+		size = prod - cons;
+	else {
+		size = XEN_9PFS_RING_SIZE - cons;
+		size += prod;
+	}
+	return size;
+}
+
+#endif
---
+++ net/9p/trans_xen.c
@@ -0,0 +1,409 @@
+/*
+ * linux/fs/9p/trans_xen
+ *
+ * Xen transport layer.
+ *
+ * Copyright (C) 2016 by Stefano Stabellini <stefano@aporeto.com>
+ */ 
+
+#include <xen/events.h>
+#include <xen/grant_table.h>
+#include <xen/xen.h>
+#include <xen/xenbus.h>
+#include <xen/interface/io/9pfs.h>
+
+#include <linux/module.h>
+#include <linux/spinlock.h>
+#include <net/9p/9p.h>
+#include <net/9p/client.h>
+#include <net/9p/transport.h>
+
+#define XEN_9PFS_NUM_RINGS 2
+
+struct xen_9pfs_dataring {
+	struct xen_9pfs_front_priv *priv;
+
+	struct xen_9pfs_data_intf *intf;
+	grant_ref_t ref;
+	int evtchn;
+	int irq;
+	spinlock_t lock;
+
+	void *bytes;
+	struct xen_9pfs_data ring;
+	wait_queue_head_t wq;
+	struct work_struct work;
+};
+
+struct xen_9pfs_front_priv {
+	struct list_head list;
+	struct xenbus_device *dev;
+	char *tag;
+	struct p9_client *client;
+
+	int num_rings;
+	struct xen_9pfs_dataring *rings;
+};
+static LIST_HEAD(xen_9pfs_devs);
+
+
+/* We don't currently allow canceling of virtio requests */
+static int p9_xen_cancel(struct p9_client *client, struct p9_req_t *req)
+{
+	return 1;
+}
+
+static int p9_xen_create(struct p9_client *client, const char *addr, char *args)
+{
+	struct xen_9pfs_front_priv *priv = NULL;
+
+	list_for_each_entry(priv, &xen_9pfs_devs, list) {
+		if (!strcmp(priv->tag, addr))
+			break;
+	}
+	if (strcmp(priv->tag, addr))
+		return -EINVAL;
+
+	priv->client = client; 
+	return 0;
+}
+
+static void p9_xen_close(struct p9_client *client)
+{
+}
+
+static int p9_xen_write_todo(struct xen_9pfs_dataring *ring, XEN_9PFS_RING_IDX size)
+{
+	XEN_9PFS_RING_IDX cons, prod;
+
+	cons = ring->intf->out_cons;
+	prod = ring->intf->out_prod;
+	mb();
+
+	if (XEN_9PFS_RING_SIZE - xen_9pfs_ring_queued(prod, cons) >= size)
+		return 1;
+	else
+		return 0;
+}
+
+static int p9_xen_request(struct p9_client *client, struct p9_req_t *p9_req)
+{
+	struct xen_9pfs_front_priv *priv = NULL;
+	XEN_9PFS_RING_IDX cons, prod, masked_cons, masked_prod;
+	unsigned long flags;
+	uint32_t size = p9_req->tc->size;
+	struct xen_9pfs_dataring *ring;
+	int num;
+
+	list_for_each_entry(priv, &xen_9pfs_devs, list) {
+		if (priv->client == client)
+			break;
+	}
+	if (priv == NULL || priv->client != client)
+		return -EINVAL;
+
+	num = p9_req->tc->tag % priv->num_rings;
+	ring = &priv->rings[num];
+
+again:
+	while (wait_event_interruptible(ring->wq,
+				p9_xen_write_todo(ring, size) > 0) != 0);
+
+	spin_lock_irqsave(&ring->lock, flags);
+	cons = ring->intf->out_cons;
+	prod = ring->intf->out_prod;
+	mb();
+
+	if (XEN_9PFS_RING_SIZE - xen_9pfs_ring_queued(prod, cons) < size) {
+		spin_unlock_irqrestore(&ring->lock, flags);
+		goto again;
+	}
+
+	masked_prod = _MASK_XEN_9PFS_IDX(prod);
+	masked_cons = _MASK_XEN_9PFS_IDX(cons);
+
+	if (masked_prod < masked_cons) {
+		memcpy(ring->ring.out + masked_prod, p9_req->tc->sdata, size);
+	} else {
+		if (size > XEN_9PFS_RING_SIZE - masked_prod) {
+			memcpy(ring->ring.out + masked_prod, p9_req->tc->sdata, XEN_9PFS_RING_SIZE - masked_prod);
+			memcpy(ring->ring.out, p9_req->tc->sdata + XEN_9PFS_RING_SIZE - masked_prod,
+					size - (XEN_9PFS_RING_SIZE - masked_prod));
+		} else {
+			memcpy(ring->ring.out + masked_prod, p9_req->tc->sdata, size );
+		}
+	}
+
+	p9_req->status = REQ_STATUS_SENT;
+	wmb();			/* write ring before updating pointer */
+	ring->intf->out_prod += size;
+	spin_unlock_irqrestore(&ring->lock, flags);
+	notify_remote_via_irq(ring->irq);
+
+	return 0;
+}
+
+static void p9_xen_response(struct work_struct *work)
+{
+	struct xen_9pfs_front_priv *priv;
+	struct xen_9pfs_dataring *ring;
+	XEN_9PFS_RING_IDX cons, prod, masked_cons, masked_prod;
+	struct xen_9pfs_header h;
+	struct p9_req_t *req;
+	int status = REQ_STATUS_ERROR;
+
+	ring = container_of(work, struct xen_9pfs_dataring, work);
+	priv = ring->priv;
+
+	while (1) {
+		cons = ring->intf->in_cons;
+		prod = ring->intf->in_prod;
+		mb();
+
+		if (xen_9pfs_ring_queued(prod, cons) < sizeof(h)) {
+			notify_remote_via_irq(ring->irq);
+			return;
+		}
+
+		masked_prod = _MASK_XEN_9PFS_IDX(prod);
+		masked_cons = _MASK_XEN_9PFS_IDX(cons);
+
+		xen_9pfs_read_header(ring->ring.in, &masked_prod, &masked_cons, &h);
+
+		req = p9_tag_lookup(priv->client, h.tag);
+		if (!req || req->status != REQ_STATUS_SENT) {
+			printk("DEBUG %s %d wrong req tag=%x\n",__func__,__LINE__,h.tag);
+			cons += h.size;
+			ring->intf->in_cons = cons;
+			wmb();
+			continue;
+		}
+
+		memcpy(req->rc, &h, sizeof(h));
+		req->rc->offset = 0;
+
+		masked_cons = _MASK_XEN_9PFS_IDX(cons);
+
+		if (masked_prod > masked_cons) {
+			memcpy(req->rc->sdata, ring->ring.in + masked_cons, h.size);
+		} else {
+			if (h.size > (XEN_9PFS_RING_SIZE - masked_cons)) {
+				memcpy(req->rc->sdata, ring->ring.in + masked_cons, XEN_9PFS_RING_SIZE - masked_cons);
+				memcpy(req->rc->sdata + XEN_9PFS_RING_SIZE - masked_cons, ring->ring.in, h.size - (XEN_9PFS_RING_SIZE - masked_cons));
+			} else {
+				memcpy(req->rc->sdata, ring->ring.in + masked_cons, h.size);
+			}
+		}
+		ring->intf->in_cons += h.size;
+		wmb();
+
+		if (req->status != REQ_STATUS_ERROR)
+			status = REQ_STATUS_RCVD;
+
+		p9_client_cb(priv->client, req, status);
+	}
+}
+
+static irqreturn_t xen_9pfs_front_event_handler(int irq, void *r)
+{
+	struct xen_9pfs_dataring *ring = r;
+
+	if (!ring)
+		return IRQ_HANDLED;
+	if (!ring->priv->client)
+		return IRQ_HANDLED;
+
+	wake_up_interruptible(&ring->wq);
+
+	schedule_work(&ring->work);
+
+	return IRQ_HANDLED;
+}
+
+static struct p9_trans_module p9_xen_trans = {
+	.name = "xen",
+	.maxsize = XEN_9PFS_RING_SIZE,
+	.def = 1,
+	.create = p9_xen_create,
+	.close = p9_xen_close,
+	.request = p9_xen_request,
+	.cancel = p9_xen_cancel,
+	.owner = THIS_MODULE,
+};
+
+static const struct xenbus_device_id xen_9pfs_front_ids[] = {
+	{ "9pfs" },
+	{ "" }
+};
+
+static int xen_9pfs_front_remove(struct xenbus_device *dev)
+{
+	return -1;
+}
+
+
+static int xen_9pfs_front_alloc_dataring(struct xenbus_device *dev, struct xen_9pfs_dataring *ring)
+{
+	int i;
+	int ret = -EFAULT;
+
+	init_waitqueue_head(&ring->wq);
+	spin_lock_init(&ring->lock);
+	INIT_WORK(&ring->work, p9_xen_response);
+
+	ring->intf = (struct xen_9pfs_data_intf *) __get_free_page(GFP_KERNEL | __GFP_ZERO);
+	if (!ring->intf)
+		goto error;
+	memset(ring->intf, 0, XEN_PAGE_SIZE);
+	ring->bytes = (void*)__get_free_pages(GFP_KERNEL | __GFP_ZERO, XEN_9PFS_RING_ORDER);
+	if (ring->bytes == NULL) {
+		ret = -ENOMEM;
+		goto error;
+	}
+	for (i = 0; i < (1 << XEN_9PFS_RING_ORDER); i++)
+		ring->intf->ref[i] = gnttab_grant_foreign_access(dev->otherend_id, pfn_to_gfn(virt_to_pfn((void*)ring->bytes) + i), 0);
+	ring->ref = gnttab_grant_foreign_access(dev->otherend_id, pfn_to_gfn(virt_to_pfn((void*)ring->intf)), 0);
+	ring->ring.in = ring->bytes;
+	ring->ring.out = ring->bytes + XEN_9PFS_RING_SIZE;
+
+	ret = xenbus_alloc_evtchn(dev, &ring->evtchn);
+	if (ret)
+		goto error;
+	ring->irq = bind_evtchn_to_irqhandler(ring->evtchn, xen_9pfs_front_event_handler,
+					0, "xen_9pfs-frontend", ring);
+	if (ring->irq < 0) {
+		ret = ring->irq;
+		goto error;
+	}
+
+error:
+	return ret;
+}
+
+static int xen_9pfs_front_probe(struct xenbus_device *dev,
+			  const struct xenbus_device_id *id)
+{
+	int ret = -EFAULT, i;
+	struct xenbus_transaction xbt;
+	struct xen_9pfs_front_priv *priv = NULL;
+
+	priv = kzalloc(sizeof(struct xen_9pfs_front_priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+	
+	priv->dev = dev;
+	priv->num_rings = XEN_9PFS_NUM_RINGS;
+	priv->rings = kzalloc(sizeof(struct xen_9pfs_dataring) * priv->num_rings, GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+ again:
+	ret = xenbus_transaction_start(&xbt);
+	if (ret) {
+		xenbus_dev_fatal(dev, ret, "starting transaction");
+		goto error;
+	}
+	ret = xenbus_printf(xbt, dev->nodename, "num-rings", "%u", priv->num_rings);
+	if (ret)
+		goto error_xenbus;
+	for (i = 0; i < priv->num_rings; i++) {
+		char str[16];
+
+		priv->rings[i].priv = priv;
+		ret = xen_9pfs_front_alloc_dataring(dev, &priv->rings[i]);
+		if (ret < 0)
+			goto error_xenbus;
+
+		sprintf(str, "ring-ref-%u", i);
+		ret = xenbus_printf(xbt, dev->nodename, str, "%d", priv->rings[i].ref);
+		if (ret)
+			goto error_xenbus;
+
+		sprintf(str, "port-%u", i);
+		ret = xenbus_printf(xbt, dev->nodename, str, "%u", priv->rings[i].evtchn);
+		if (ret)
+			goto error_xenbus;
+	}
+	priv->tag = xenbus_read(xbt, dev->nodename, "tag", NULL);
+	if (ret)
+		goto error_xenbus;
+	ret = xenbus_transaction_end(xbt, 0);
+	if (ret) {
+		if (ret == -EAGAIN)
+			goto again;
+		xenbus_dev_fatal(dev, ret, "completing transaction");
+		goto error;
+	}
+
+	v9fs_register_trans(&p9_xen_trans);
+
+	list_add_tail(&priv->list, &xen_9pfs_devs);
+	dev_set_drvdata(&dev->dev, priv);
+	xenbus_switch_state(dev, XenbusStateInitialised);
+
+	return 0;
+
+ error_xenbus:
+	xenbus_transaction_end(xbt, 1);
+	xenbus_dev_fatal(dev, ret, "writing xenstore");
+ error:
+	xen_9pfs_front_remove(dev);
+	return ret;
+}
+
+static int xen_9pfs_front_resume(struct xenbus_device *dev)
+{
+	dev_warn(&dev->dev, "suspsend/resume unsupported\n");
+	return 0;
+}
+
+static void xen_9pfs_front_changed(struct xenbus_device *dev,
+			    enum xenbus_state backend_state)
+{
+	switch (backend_state) {
+	case XenbusStateReconfiguring:
+	case XenbusStateReconfigured:
+	case XenbusStateInitialising:
+	case XenbusStateInitialised:
+	case XenbusStateUnknown:
+		break;
+
+	case XenbusStateInitWait:
+		break;
+
+	case XenbusStateConnected:
+		xenbus_switch_state(dev, XenbusStateConnected);
+		break;
+
+	case XenbusStateClosed:
+		if (dev->state == XenbusStateClosed)
+			break;
+		/* Missed the backend's CLOSING state -- fallthrough */
+	case XenbusStateClosing:
+		xenbus_frontend_closed(dev);
+		break;
+	}
+}
+static struct xenbus_driver xen_9pfs_front_driver = {
+	.ids = xen_9pfs_front_ids,
+	.probe = xen_9pfs_front_probe,
+	.remove = xen_9pfs_front_remove,
+	.resume = xen_9pfs_front_resume,
+	.otherend_changed = xen_9pfs_front_changed,
+};
+
+int p9_trans_xen_init(void)
+{
+	if (!xen_domain())
+		return -ENODEV;
+
+	pr_info("Initialising Xen virtual socket driver\n");
+
+	return xenbus_register_frontend(&xen_9pfs_front_driver);
+}
+module_init(p9_trans_xen_init);
+
+void p9_trans_xen_exit(void)
+{
+	v9fs_unregister_trans(&p9_xen_trans);
+}
+module_exit(p9_trans_xen_exit);
